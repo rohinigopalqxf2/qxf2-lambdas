55c05ea150480b079fad177e566322b4580090e1
Qxf2 <mak@qxf2.com>
2020-09-15 02:47:12 +0530

    Added a simple test for daily_messages.write_message() as an example of mocking SQS

---
+++ tests/test_daily_messages.py
@@ -0,0 +1,20 @@
+"""
+Code level tests for the daily messages lambda
+"""
+import boto3
+from moto import mock_sqs
+from daily_messages import daily_messages
+
+@mock_sqs
+def test_write_message_valid():
+    "Test the write_message method with a valid message"
+    sqs = boto3.resource('sqs')
+    queue = sqs.create_queue(QueueName='test-skype-sender')
+    daily_messages.QUEUE_URL = queue.url
+    skype_message = 'Testing with a valid message'
+    channel = 'test'
+    expected_message = str({'msg':f'{skype_message}', 'channel':channel})
+    daily_messages.write_message(skype_message, channel)
+    sqs_messages = queue.receive_messages()
+    assert len(sqs_messages) == 1
+    assert sqs_messages[0].body == expected_message
